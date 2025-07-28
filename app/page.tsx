53eaae8d210f552a906f02f4e40fd594e5d2e52b
Yamei Song <ymsongx@163.com>
2025-07-28 16:47:19 +0800

    Add AccountCard component to Home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import StockList from "./components/StockList";
 import PriceTrend from "./components/PriceTrend";
 import PortfolioList from "./components/PortfolioList";
 import Navbar from "./components/Navbar";
+import AccountCard from "./components/AccountCard";
 
 export default function Home() {
   const [trendSymbol, setTrendSymbol] = React.useState('AAPL');
@@ -12,7 +13,8 @@ export default function Home() {
     <div>
       <Navbar/>
       <div className="container mx-auto p-4 flex">
-        <div className="w-1/2 pr-2">
+        <div className="w-1/2 pr-2 flex flex-col gap-4">
+          <AccountCard />
           <PriceTrend symbol={trendSymbol} />
         </div>
         <div className="w-1/2 pl-2">
