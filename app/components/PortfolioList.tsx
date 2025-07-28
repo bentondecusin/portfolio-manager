d6c9e01dcfa67f653a23bee7da37db41dc0682de
Benton Li <cl2597@cornell.edu>
2025-07-28 17:16:32 +0800

    Wrap tables

--- app/components/PortfolioList.tsx
+++ app/components/PortfolioList.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState, useEffect } from 'react'
 import {
     Table,
@@ -71,5 +72,80 @@ const PortfolioList = () => {
         </div>
     )
 }
+=======
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import TableWrapper from "../table_wrapper";
 
-export default PortfolioList
+const portfolios = [
+  {
+    name: "Apple Inc.",
+    symbol: "AAPL",
+    shares: 10,
+    price: "$150.00",
+    totalReturn: "$1500.00",
+  },
+  {
+    name: "Alphabet Inc.",
+    symbol: "GOOGL",
+    shares: 5,
+    price: "$2800.00",
+    totalReturn: "$14000.00",
+  },
+  {
+    name: "Amazon.com Inc.",
+    symbol: "AMZN",
+    shares: 2,
+    price: "$3400.00",
+    totalReturn: "$6800.00",
+  },
+  {
+    name: "Microsoft Corporation",
+    symbol: "MSFT",
+    shares: 8,
+    price: "$299.00",
+    totalReturn: "$2392.00",
+  },
+];
+
+const PortfolioList = () => {
+  return (
+    <TableWrapper>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Name</TableHead>
+            <TableHead>Symbol</TableHead>
+            <TableHead>Shares</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead className="text-right">Total Return</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {portfolios.map((portfolio) => (
+            <TableRow key={portfolio.name}>
+              <TableCell className="font-medium">{portfolio.name}</TableCell>
+              <TableCell>{portfolio.symbol}</TableCell>
+              <TableCell>{portfolio.shares}</TableCell>
+              <TableCell>{portfolio.price}</TableCell>
+              <TableCell className="text-right">
+                {portfolio.totalReturn}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableWrapper>
+  );
+};
+>>>>>>> Stashed changes
+
+export default PortfolioList;
