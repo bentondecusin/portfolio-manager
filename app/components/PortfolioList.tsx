be285665237cff95b3c24ef6faafc2285f5b017d
ChelseaWang05 <145506108+ChelseaWang05@users.noreply.github.com>
2025-07-28 16:52:32 +0800

    add portfolioList

--- app/components/PortfolioList.tsx
+++ app/components/PortfolioList.tsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
     Table,
     TableBody,
-    TableCaption,
     TableCell,
     TableHead,
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
 
-const portfolios = [
+type Portfolio = {
+    name: string,
+    symbol: string,
+    shares: number,
+    price: string,
+    totalReturn: string
+}
+
+const portfolios_test = [
     { name: 'Apple Inc.', symbol: 'AAPL', shares: 10, price: '$150.00', totalReturn: '$1500.00' },
     { name: 'Alphabet Inc.', symbol: 'GOOGL', shares: 5, price: '$2800.00', totalReturn: '$14000.00' },
     { name: 'Amazon.com Inc.', symbol: 'AMZN', shares: 2, price: '$3400.00', totalReturn: '$6800.00' },
@@ -17,6 +24,26 @@ const portfolios = [
 ];
 
 const PortfolioList = () => {
+    const [portfolios, setPortfolios] = useState<Portfolio[]>([]);
+
+    useEffect(() => {
+        // const fetchPortfolios = async () => {
+        //     try {
+        //         const res = await fetch('/api/portfolios');
+        //         const data = await res.json();
+        //         setPortfolios(data);
+        //     } catch (error) {
+        //         console.error('Failed to fetch portfolios:', error);
+        //     } 
+        // };
+
+        // fetchPortfolios();
+
+        setPortfolios(portfolios_test);
+    }, []); 
+
+    // if (loading) return <div>Loading...</div>;
+
     return (
         <div>
             <Table>
