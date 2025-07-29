f9a1c21d61a8c52bf1d33c988062ca9b2bcff490
Kevin Wang <zhenglin_kevin@qq.com>
2025-07-29 10:31:28 +0800

    delete some file

--- app/db.ts
+++ app/db.ts
@@ -1,14 +1,16 @@
+// db.ts
 const mysql = require('mysql2/promise')
 
-// Create DB pool
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'n3u3da!',
+  password: 'QQW1372zl.',
+  port: 3306,
   database: 'portfolio_management',
   waitForConnections: true,
   connectionLimit: 10,
-  timezone: '+08:00'
-})
+  queueLimit: 0,
+  timezone: '+08:00', // Optional, based on your region
+});
 
-export default db
+export default db;
