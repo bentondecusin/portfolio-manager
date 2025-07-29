9f13c884d8be06d8eaf638d2761bad39eb24ef5e
syh369 <shenyuanhao1999@gmail.com>
2025-07-30 00:14:36 +0800

    Added transactions crud and integrity check logics

--- BACKEND/src/route/transactionRoute.js
+++ BACKEND/src/route/transactionRoute.js
@@ -4,7 +4,7 @@ const ctrl = require('../controller/transactionController');
 const router = express.Router();
 
 router.get('/', ctrl.getAllTxn);
-// router.post('/transactions', ctrl.postTransaction);
+router.post('/transactions', ctrl.postTransaction);
 // router.get('/portfolio/live', ctrl.getPortfolioLive);
 
 module.exports = router;
