b25f1d6e28965abd093a49159ffadeadb6c7322c
Yamei Song <ymsongx@163.com>
2025-07-29 19:36:53 +0800

    Remove yahoo-finance2 dependency, fix duplicate backend entries, and add new asset-related files

--- backend/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config(); 
-const app = require("./src/route/index.js");
-const PORT = 8080;
-
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
-});
--- backend/src/service/transactionService.js
+++
@@ -1,8 +0,0 @@
-const txnModel = require('../model/transactionModel');
-
-async function getTxnHistory() {
-    const txn_list = await txnModel.listAll();
-    return txn_list;
-}
-
-module.exports = { getTxnHistory };
